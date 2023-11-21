aff60686e7132fe278b04856b3ce14f6e3b108e0
student31 <student31@student31.local>
2023-11-21 18:45:57 +0900

    slider

---
+++ app/components/Slider.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Slider({ children }) {
+    const [position, setPosition] = useState(0);
+
+    const totalSlides = 5;
+
+    // 랜덤 색상 생성 함수
+    const generateRandomColor = () => {
+        return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    };
+
+    // 색상을 담을 배열 생성
+    const slideColors = Array.from({ length: totalSlides }, generateRandomColor);
+
+    const slideLeft = () => {
+        setPosition(prevPosition => prevPosition > 0 ? prevPosition - 1 : 0);
+    };
+
+    const slideRight = () => {
+        setPosition(prevPosition => prevPosition < totalSlides - 1 ? prevPosition + 1 : totalSlides - 1);
+    };
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+        <div className="w-2/5 h-3/5" style={{ backgroundColor: slideColors[position] }}>
+            <button onClick={slideLeft}>Left</button>
+            <button onClick={slideRight}>Right</button>
+            <div>Position: {position + 1}</div> {/* 슬라이드 번호를 1부터 시작하도록 표시 */}
+        </div>
+        </div>
+  );
+    }
--- app/random/page.js
+++ app/random/page.js
@@ -1,10 +1,17 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+
 import SlideInput from '../components/SlideInput';
+import Slider from '../components/Slider';
 
 export default function Random({ children }) {
+    const [slides, setSlides] = useState({});
+
     return (
         <div>
-        <h1>Slide Input</h1>
-        <SlideInput />
+        <SlideInput setSlides={setSlides} />
+        <Slider slides={slides} />
         </div>
     )
   }
